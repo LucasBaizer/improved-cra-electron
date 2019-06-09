f428d08e68edfb3843002bd7ac38072fb51d246e
Lucas Baizer <titaniumsapphire32@gmail.com>
2019-06-09 11:23:13 -0700

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './ui/App';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+	<Router history={history}>
+		<Switch>
+			<Route exact path="/" component={App} />
+		</Switch>
+	</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
